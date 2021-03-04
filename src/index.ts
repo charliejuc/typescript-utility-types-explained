07e8217c8124754c9c7319af0fa6861b505b6af5
charliejuc <charliejuc@gmail.com>
2021-03-04 16:25:26 +0100

    Record

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,21 @@
 const tuple1: [number, string, number, number] = [1, 'hey', 3, 4]
-const obj1 = {
+
+interface ObjScore {
+    id: number
+    name: string
+    score: number
+}
+
+const obj1: ObjScore & {
+    keys(): Array<keyof ObjScore>
+} = {
     id: 1,
     name: 'John',
-    score: 45
+    score: 45,
+    // Explain: Object.keys issue
+    keys(): ReturnType<typeof obj1.keys> {
+        return ['id', 'name', 'score']
+    }
 }
 
 class MyClass {
@@ -10,18 +23,48 @@ class MyClass {
     y = 5
 }
 
-type CustomReadonly<T> = {
-    readonly [K in keyof T]: T[K]
+type CustomRecord<K extends keyof any, T> = {
+    [P in K]: T
 }
 
-const roObj1: Readonly<typeof obj1> = obj1
-const roTuple1: Readonly<typeof tuple1> = tuple1
+const a: keyof any = 'sdfsda'
 
-roObj1.id = 5
-roTuple1[0] = 7
+const rTuple1: Record<number, number | string> = tuple1
+tuple1[0] = 1
 
-const romcInstance: Readonly<MyClass> = new MyClass()
-const rocmcInstance: CustomReadonly<MyClass> = romcInstance
+const rObj1: Record<string, unknown> = obj1
+const rObj2: Record<string, unknown> = { ...obj1 }
+const rObj3: CustomRecord<string, unknown> = { ...obj1 }
 
-romcInstance.y = 6
-rocmcInstance.x = 8
+// number as key is computed as a string
+rObj3[4] = 'fsdf'
+rObj3['fsdaf'] = 'fsdf'
+
+const k = obj1.keys()
+
+obj1[k[0]]
+obj1['sdfsd']
+
+type ValidKeys = 'first' | 'second' | 'third'
+const a: Record<ValidKeys, number> = {
+    first: 2,
+    second: 7,
+    third: 10,
+    fourth: 8
+}
+
+type Dictionary<K extends string | number | symbol, T> = Partial<Record<K, T>>
+
+const r: Record<string, number> = {}
+const d: Dictionary<string, number> = {}
+const ru: Record<string, unknown> = {}
+
+const returnNumber = (n: number): number => n
+
+returnNumber(r.hola)
+
+returnNumber(d.hola)
+returnNumber(d.hola ?? 1)
+
+returnNumber(ru.hola)
+returnNumber(typeof ru.hola === 'number' ? ru.hola : 1)
