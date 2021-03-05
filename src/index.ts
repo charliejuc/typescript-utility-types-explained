2e04090afdb9502d3268d6e813c167bd8e39640f
charliejuc <charliejuc@gmail.com>
2021-03-05 11:50:10 +0100

    Omit

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,6 @@ const obj1: ObjScore & {
     id: 1,
     name: 'John',
     score: 45,
-    // Explain: Object.keys issue
     keys(): ReturnType<typeof obj1.keys> {
         return ['id', 'name', 'score']
     }
@@ -23,18 +22,15 @@ class MyClass {
     y = 5
 }
 
+type CustomPick<T, K extends keyof T> = {
+    [P in K]: T[P]
+}
 type CustomExtract<T, U> = T extends U ? T : never
 type CustomExclude<T, U> = T extends U ? never : T
 
-type U1 = 'a' | 'b' | 'c'
-type U2 = 'x' | 'z' | 'b'
-type U3 = 'c' | 'z' | 'b'
+type CustomOmit<T, K extends keyof T> = CustomPick<T, CustomExclude<keyof T, K>>
 
-const a: CustomExtract<U1, U2>
-const b: CustomExclude<U1, U2>
-
-const c: CustomExtract<U2, U3>
-const d: CustomExclude<U2, U3>
-
-const e: CustomExtract<U3, U2>
-const f: CustomExclude<U3, U2>
+const a: CustomOmit<ObjScore, 'score' | 'id'> = {
+    id: 1,
+    score: 33
+}
