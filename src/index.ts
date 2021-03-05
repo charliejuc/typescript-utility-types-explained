0f4e524b57f11640ca1921e03ef7c77caff4fad3
charliejuc <charliejuc@gmail.com>
2021-03-05 10:59:55 +0100

    Pick

--- src/index.ts
+++ src/index.ts
@@ -23,48 +23,15 @@ class MyClass {
     y = 5
 }
 
-type CustomRecord<K extends keyof any, T> = {
-    [P in K]: T
+type CustomPick<T, K extends keyof T> = {
+    [P in K]: T[P]
 }
 
-const a: keyof any = 'sdfsda'
-
-const rTuple1: Record<number, number | string> = tuple1
-tuple1[0] = 1
-
-const rObj1: Record<string, unknown> = obj1
-const rObj2: Record<string, unknown> = { ...obj1 }
-const rObj3: CustomRecord<string, unknown> = { ...obj1 }
-
-// number as key is computed as a string
-rObj3[4] = 'fsdf'
-rObj3['fsdaf'] = 'fsdf'
-
-const k = obj1.keys()
-
-obj1[k[0]]
-obj1['sdfsd']
-
-type ValidKeys = 'first' | 'second' | 'third'
-const a: Record<ValidKeys, number> = {
-    first: 2,
-    second: 7,
-    third: 10,
-    fourth: 8
+const a: CustomPick<ObjScore, 'score'> = {
+    score: 44,
+    name: 'fdass'
 }
-
-type Dictionary<K extends string | number | symbol, T> = Partial<Record<K, T>>
-
-const r: Record<string, number> = {}
-const d: Dictionary<string, number> = {}
-const ru: Record<string, unknown> = {}
-
-const returnNumber = (n: number): number => n
-
-returnNumber(r.hola)
-
-returnNumber(d.hola)
-returnNumber(d.hola ?? 1)
-
-returnNumber(ru.hola)
-returnNumber(typeof ru.hola === 'number' ? ru.hola : 1)
+const b: Pick<ObjScore, 'score'> = {
+    score: 44,
+    name: 'sdfasf'
+}
